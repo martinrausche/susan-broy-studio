4d40c1f669fc6deab9d249dd9f3e16495297201e
Martin Rausche <martin@Martins-MacBook-Pro-2.local>
2026-07-26 13:23:35 +0200

    Remove .next from git tracking and add to .gitignore

--- .next/static/chunks/app/layout-d7e74c6e1991c016.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1328:function(n,e,u){Promise.resolve().then(u.t.bind(u,1067,23))},1067:function(){}},function(n){n.O(0,[422,971,117,744],function(){return n(n.s=1328)}),_N_E=n.O()}]);
